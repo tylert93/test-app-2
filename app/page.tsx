db43c5d135703f6f10361f0dee390f5e0381e850
Tom Tyler <tom.tyler.dev@gmail.com>
2023-11-04 14:35:17 +0000

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            bullshit
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -111,3 +111,4 @@ export default function Home() {
     </main>
   )
 }
+Ê
