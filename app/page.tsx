16582489e85ddd750b6ac951bfff79fa6dbb51fe
Tom Tyler <tom.tyler.dev@gmail.com>
2023-11-04 14:38:53 +0000

    more changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -104,11 +104,10 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Such a load of bullshit
           </p>
         </a>
       </div>
     </main>
-  )
+  );
 }
-Ê
